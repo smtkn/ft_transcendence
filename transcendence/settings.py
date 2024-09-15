157462063d9c36bbfc6e567bf3e179a59009b2c4
smtkn <samettakan41@gmail.com>
2024-09-15 17:44:12 +0300

    Chat app'nin template hazırlandı

--- transcendence/settings.py
+++ transcendence/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
     'dashboard.apps.DashboardConfig',
+    'chat.apps.ChatConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- transcendence/urls.py
+++ transcendence/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('dashboard/', include('dashboard.urls')),
+    path('chat/', include('chat.urls')),
 ]
