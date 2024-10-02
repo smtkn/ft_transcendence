d682679e88f3c56d7adc6312f673d058e889225b
smtkn <samettakan41@gmail.com>
2024-10-02 21:18:11 +0300

    Ana sayfa ayarlandı.

--- home/urls.py
+++ home/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
     path('forgot-pasaword/', views.forgotPassword, name='forgot-password'),
     path('reset-password/', views.resetPassword, name='reset-password'),
-
 ]
